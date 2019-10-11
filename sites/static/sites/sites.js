7013fd3c79d9c44991f5823a3dc1f85f300c1d85
koualsky <koualsky@gmail.com>
2019-10-11 16:06:57 +0200

    Add animation to the logo

--- sites/static/sites/sites.js
+++ sites/static/sites/sites.js
@@ -1,13 +1,29 @@
-// MAIN
-window.onload = function() {
-    try {
+// LOGO ANIMATE
+const logo = document.getElementById('logo');
 
-        // somefunction();
+logo.onmouseover = function() {
 
-    } catch (error) {
-        console.log(error);
+    var numb = Math.floor(Math.random() * 10);
+
+    var list = [
+        'bounce', 
+        'flash', 
+        'pulse', 
+        'rubberBand', 
+        'shake', 
+        'swing', 
+        'tada', 
+        'wobble', 
+        'jello', 
+        'heartBeat'
+    ];
+
+    var random = list[numb];
+
+    function deleteAnimate() {
+        logo.classList.remove("animated", random);
     }
+
+    logo.classList.add("animated", random);
+    setTimeout(deleteAnimate, 1000);
 }
-
-
-// FUNCTIONS
